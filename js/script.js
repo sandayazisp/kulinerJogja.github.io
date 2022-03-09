f6c0e77d713a1315b7b316b509f1291c798fcc3f
sandayazisp <sandayazisp12@gmail.com>
2022-03-09 20:49:26 +0700

    menambahkan script untuk melakukan interaksi pada toggle di index.html

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.sidenav').sidenav();
 });
+
+const checkbox = document.querySelector('#toggle');
+const body = document.querySelector('body');
+
+if (localStorage.getItem('theme') == 'dark') {    
+    setDarkMode();   
+}
+
+function setDarkMode() {    
+    checkbox.setAttribute('checked', 'checked');
+    if (checkbox.checked) {
+        body.classList.add('dark');         
+        localStorage.setItem('theme', 'dark');
+    }else{
+        body.classList.remove('dark');
+        localStorage.removeItem('theme');
+    }
+    
+}
+
+checkbox.addEventListener('click', setDarkMode);
+
